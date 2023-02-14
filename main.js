a43c7f89f75ff6847ec8cdc23c679e293e0de7af
AbaevB <abaev70@hotmail.com>
2023-02-15 00:05:32 +0700

    feat(js): add functions
    functions createAppTitle and createTodoItemForm added

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
   /**Создаем и
    * возвращаем заголовок приложения
    */
-  function createAppTitle(){
-
+  function createAppTitle(title){
+    let appTitle = document.createElement('h2');
+    appTitle.innerHTML = tile;
+    return appTitle;
   }
 
   /**
@@ -12,7 +14,27 @@
    * дела
    */
   function createTodoItemForm(){
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    let buuttonWrapper = document.createElement('div');
+    let button = createElement('button');
 
+    form.classList.add('input-group', 'mb-3');
+    input.classList.add('form-control');
+    input.placeholder = 'Введите название нового дела';
+    buuttonWrapper.classList.add('input-group-append');
+    button.classList.add('btn', 'btn-primary');
+    button.textContent = 'Добавить дело';
+
+    buuttonWrapper.append(button);
+    form.append(input);
+    form.append(buuttonWrapper);
+
+    return {
+      form,
+      input,
+      button,
+    };
   }
 
   /**Создаем и возвращаем
